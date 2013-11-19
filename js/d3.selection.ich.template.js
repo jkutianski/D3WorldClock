ab7b24ff083e20a6621d05a959ebfd6053fd7eca
jkutianski <jkutianski@gmail.com>
2013-11-19 20:58:18 -0300

    new version

--- js/d3.selection.ich.template.js
+++
@@ -1,6 +0,0 @@
-d3.selection.prototype.ich = d3.selection.enter.prototype.ich = function(id,d) {
-  var thisSelection = (d) ? this.datum(d) : this;
-  thisSelection.html(function (d) {
-                  return ich[id](d);
-                });
-};
